8e1eecafc38b2ffa2c03df55df3be5eaed74ae8a
gg.lyon <gioloayon@gmail.com>
2021-05-17 17:26:55 +0300

    improved frontend

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -56,7 +56,7 @@ class MoviesController < ApplicationController
   def destroy
     @movie.destroy
     respond_to do |format|
-      format.html { redirect_to movies_url, notice: "Movie was successfully destroyed." }
+      format.html { redirect_to movies_url, notice: "Movie was successfully deleted." }
       format.json { head :no_content }
     end
   end
